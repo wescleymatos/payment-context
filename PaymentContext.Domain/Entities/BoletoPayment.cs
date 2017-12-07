c525728bc027ea27cf881eeebeeffae699bb01a9
Wescley Matos <wescleymatos@gmail.com>
2017-12-07 13:41:06 -0300

    organização de arquivos

---
+++ PaymentContext.Domain/Entities/BoletoPayment.cs
@@ -0,0 +1,8 @@
+namespace PaymentContext.Domain.Entities
+{
+    public class BoletoPayment : Payment
+    {
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
+    }
+}
--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -14,22 +14,4 @@ namespace PaymentContext.Domain.Entities
         public string Address { get; set; }
         public string Email { get; set; }
     }
-
-    public class BoletoPayment : Payment
-    {
-        public string BarCode { get; set; }
-        public string BoletoNumber { get; set; }
-    }
-
-    public class CreditCardPayment : Payment
-    {
-        public string CardHolderName { get; set; }
-        public string CardNumber { get; set; }
-        public string LastTransactionNumber { get; set; }
-    }
-
-    public class PayPalPayment : Payment
-    {
-        public string TransactionCode { get; set; }
-    }
 }
