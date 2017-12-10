2efe9b2ccc168c2b0fe9cf9db807933683b3838e
Wescley Matos <wescleymatos@gmail.com>
2017-12-09 22:07:22 -0300

    Criação de teste para o cammand de pagamento com boleto

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,9 +1,12 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -25,5 +28,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres.")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres.")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome não deve conter pelo mais que 40 caracteres.")
+            );
+        }
     }   
 }
